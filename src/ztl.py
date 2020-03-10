1f05faa8a345bda231da98fce1dd47fe174a7ad0
sptkl <caobaiyue@gmail.com>
2020-03-10 14:17:10 -0400

    adding tool tips, temporary solution used

--- src/ztl.py
+++ src/ztl.py
@@ -5,19 +5,31 @@ def ztl():
     import requests
     import os 
     import pydeck as pdk
-    import json 
+    import json
 
     @st.cache(suppress_st_warning=True, allow_output_mutation=True)
     def get_data():
         source_data_versions=pd.read_csv('https://edm-publishing.nyc3.cdn.digitaloceanspaces.com/db-zoningtaxlots/latest/output/source_data_versions.csv')
-        bbldiff=pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_bbldiffs.csv')
+        
+        bbldiff=pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_bbldiffs.csv', dtype=str)
+        bbldiff = bbldiff.fillna('NULL')
+        bbldiff['longitude'] = bbldiff.longitude.astype(float)
+        bbldiff['latitude'] = bbldiff.latitude.astype(float)
+        
         last_build = requests.get('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/version.txt').text
+        
         qc_frequencychanges = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_frequencychanges.csv')
+        
         qc_versioncomparison = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_versioncomparison.csv')
+        
         qc_versioncomparisonnownullcount = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_versioncomparisonnownullcount.csv')
+        
         qc_bbls_count_added_removed = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_bbls_count_added_removed.csv')
+        
         DATA_URL='https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_bbldiffs.geojson'
+        
         geojson = requests.get(DATA_URL).json()
+        
         return source_data_versions, bbldiff, last_build, qc_frequencychanges, qc_versioncomparison, qc_versioncomparisonnownullcount, qc_bbls_count_added_removed, geojson
     
     source_data_versions, bbldiff, last_build, qc_frequencychanges, qc_versioncomparison, qc_versioncomparisonnownullcount, qc_bbls_count_added_removed, geojson = get_data()
@@ -36,7 +48,7 @@ def ztl():
     st.dataframe(df)
 
     st.pydeck_chart(pdk.Deck(
-        map_style='mapbox://styles/mapbox/light-v9',
+        map_style='mapbox://styles/mapbox/dark-v9',
         initial_view_state=pdk.ViewState(
             latitude=40.7128,
             longitude=-74.0006,
@@ -47,27 +59,45 @@ def ztl():
             pdk.Layer(
                 'GeoJsonLayer',
                 data=geojson,
-                opacity=0.8,
-                pickable=True,
-                wireframe=True,
-                stroked=False,
-                filled=True,
+                opacity=0.5,
+                pickable=False,
+                filled=False,
                 auto_highlight=True,
-                get_line_color=[255, 255, 255]
-            )
+                get_fill_color='[225, 225, 0, 225]',
+            ), 
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=bbldiff,
+                radius=200,
+                get_position='[longitude, latitude]',
+                pickable=True,
+                get_color='[200, 30, 0, 160]',
+            ),
         ],
-        # tooltip={
-        #     "html": '''
-        #      <div><b>BBL &nbsp;</b></div>
-        #     <div><div>${object.properties.area}</div></div>
-        #     <div><b>Growth</b></div>
-        #     <div>${properties}%</div>
-        #     ''',
-        #     "style": {
-        #             "backgroundColor": "steelblue",
-        #             "color": "white"
-        #     }
-        # }
+        tooltip={
+            "html": '''
+                <div><b>BBL: {bblnew} &nbsp;</b></div>
+                <ul>
+                <li>zd1new: {zd1new}  zd1prev: {zd1prev}</li>
+                <li>zd2new: {zd2new}  zd2prev: {zd2prev}</li>
+                <li>zd3new: {zd3new}  zd3prev: {zd3prev}</li>
+                <li>zd4new: {zd4new}  zd4prev: {zd4prev}</li>
+                <li>co1new: {co1new}  co1prev: {co1prev}</li>
+                <li>co2new: {co2new}  co2prev: {co2prev}</li>
+                <li>sd1new: {sd1new}  sd1prev: {sd1prev}</li>
+                <li>sd2new: {sd2new}  sd2prev: {sd2prev}</li>
+                <li>sd3new: {sd3new}  sd3prev: {sd3prev}</li>
+                <li>lhdnew: {lhdnew}  lhdprev: {lhdprev}</li>
+                <li>zmnnew: {zmnnew}  zmnprev: {zmnprev}</li>
+                <li>zmcnew: {zmcnew}  lhdprev: {lhdprev}</li>
+                </ul>
+            ''',
+            "style": {
+                    "backgroundColor": "steelblue",
+                    "color": "white",
+                    "display" : "block"
+            }
+        }
     ))
 
     # bbl = st.selectbox('view a bbl on zola:', df.bbl.to_list())
