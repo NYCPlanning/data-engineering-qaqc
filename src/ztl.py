c8f4a3e802c19ee6afec926180d3939c690065f0
sptkl <caobaiyue@gmail.com>
2020-03-18 12:50:30 -0400

    adding zoning taxlots comments

--- src/ztl.py
+++ src/ztl.py
@@ -26,19 +26,15 @@ def ztl():
         
         qc_bbls_count_added_removed = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_bbls_count_added_removed.csv')
         
-        DATA_URL='https://edm-publishing.nyc3.digitaloceanspaces.com/db-zoningtaxlots/latest/output/qc_bbldiffs.json'
-        
-        geojson = requests.get(DATA_URL).json()
-        
-        return source_data_versions, bbldiff, last_build, qc_frequencychanges, qc_versioncomparison, qc_versioncomparisonnownullcount, qc_bbls_count_added_removed, geojson
+        return source_data_versions, bbldiff, last_build, qc_frequencychanges, qc_versioncomparison, qc_versioncomparisonnownullcount, qc_bbls_count_added_removed
     
-    source_data_versions, bbldiff, last_build, qc_frequencychanges, qc_versioncomparison, qc_versioncomparisonnownullcount, qc_bbls_count_added_removed, geojson = get_data()
+    source_data_versions, bbldiff, last_build, qc_frequencychanges, qc_versioncomparison, qc_versioncomparisonnownullcount, qc_bbls_count_added_removed = get_data()
 
     st.title('Zoning Tax Lots QAQC')
     st.markdown(f'![CI](https://github.com/NYCPlanning/db-zoningtaxlots/workflows/CI/badge.svg) last build: {last_build}')
 
     column = st.sidebar.selectbox('select column:', ['all', 'zd1', 'zd2', 'zd3', 'zd4','co1', 'co2', 'sd1', 'sd2', 'sd3','lhd'])
-    st.text('BBLs with Zoning Changes')
+    st.header('qc_bbldiffs')
     if column == 'all':
         df = bbldiff.rename(columns={'bblnew':'bbl'})
     else: 
@@ -46,80 +42,43 @@ def ztl():
         df=df.dropna(subset=[f'{column}new', f'{column}prev'])
         df.columns = ['bbl', f'{column}new', f'{column}prev']
     st.dataframe(df)
-
-    st.pydeck_chart(pdk.Deck(
-        map_style='mapbox://styles/mapbox/dark-v9',
-        initial_view_state=pdk.ViewState(
-            latitude=40.7128,
-            longitude=-74.0006,
-            zoom=11,
-            pitch=0,
-        ),
-        layers=[
-            pdk.Layer(
-                'GeoJsonLayer',
-                data=geojson,
-                opacity=0.5,
-                filled=False,
-                auto_highlight=True,
-                get_fill_color='[225, 225, 0, 225]',
-            ), 
-            pdk.Layer(
-                "ScatterplotLayer",
-                data=bbldiff,
-                opacity=0.4,
-                pickable=True,
-                get_position="[longitude, latitude]",
-                get_color="[200, 30, 0, 160]",
-                get_radius=10,
-            ),
-        ],
-        tooltip={
-            "html": '''
-                <div><b>BBL: {bblnew} &nbsp;</b></div>
-                <ul>
-                <li>zd1new: {zd1new}  zd1prev: {zd1prev}</li>
-                <li>zd2new: {zd2new}  zd2prev: {zd2prev}</li>
-                <li>zd3new: {zd3new}  zd3prev: {zd3prev}</li>
-                <li>zd4new: {zd4new}  zd4prev: {zd4prev}</li>
-                <li>co1new: {co1new}  co1prev: {co1prev}</li>
-                <li>co2new: {co2new}  co2prev: {co2prev}</li>
-                <li>sd1new: {sd1new}  sd1prev: {sd1prev}</li>
-                <li>sd2new: {sd2new}  sd2prev: {sd2prev}</li>
-                <li>sd3new: {sd3new}  sd3prev: {sd3prev}</li>
-                <li>lhdnew: {lhdnew}  lhdprev: {lhdprev}</li>
-                <li>zmnnew: {zmnnew}  zmnprev: {zmnprev}</li>
-                <li>zmcnew: {zmcnew}  lhdprev: {lhdprev}</li>
-                </ul>
-            ''',
-            "style": {
-                    "backgroundColor": "steelblue",
-                    "color": "white",
-                    "display" : "block"
-            }
-        }
-    ))
-
-    # bbl = st.selectbox('view a bbl on zola:', df.bbl.to_list())
-    # st.markdown(f'[click to view {bbl} on zola](https://zola.planning.nyc.gov/bbl/{bbl})')
-    # st.markdown(
-    #     f"""
-    #     <iframe src="https://zola.planning.nyc.gov/bbl/{bbl}" style='height:600px; width:100%'>
-    #     </iframe>
-    #     """, 
-    #     unsafe_allow_html=True)
-
-    st.header('Source Data Versions')
-    st.table(source_data_versions)
+    st.markdown('''
+    Contains the old and new values for BBLs with changes from the last version.
+    + Layer qc_bbldiffs, the new zoning shapefiles, and the current Digital tax map onto a map. 
+    + Sort qc_bbldiffs by BBL
+    + Verify that the zoning changes for each BBL meet one of the following criteria:
+        + Recent zoning change
+        + Adjustment to boundary
+        + Change in tax lot 
+    + Verify all lots in newly rezoned areas have new values
+    ''')
 
     st.header('Frequency Changes')
     st.table(qc_frequencychanges)
 
     st.header('Version Comparison')
     st.table(qc_versioncomparison)
+    st.markdown('''
+    Compares the value differences between this version and 
+    the previous version, showing the number of records with a 
+    change in value and the percentage of these fields that changed.
+    ''')
 
     st.header('Version Comparison -- Null Count')
     st.table(qc_versioncomparisonnownullcount)
+    st.markdown('''
+    reports the number of records that changed 
+    from null to a value or vice versa.
+    ''')
 
     st.header('BBLs added/removed')
     st.table(qc_bbls_count_added_removed)
+    st.markdown('''
+    Shows how many records have non-null values for each field 
+    in the old and new version. Note that changes to the number 
+    of records with a value may result from changes to null 
+    values or from BBL changes.
+    ''')
+
+    st.header('Source Data Versions')
+    st.table(source_data_versions)
