d2865569559dd28d9bacb4f8107e70340b80ac3a
sptkl <caobaiyue@gmail.com>
2020-03-26 15:43:52 -0400

    update url

--- src/devdb.py
+++ src/devdb.py
@@ -8,14 +8,14 @@ def devdb():
 
     @st.cache(suppress_st_warning=True, allow_output_mutation=True)
     def get_data():
-        u = 'https://raw.githubusercontent.com/NYCPlanning/db-developments/future/developments_build/output/'
+        u = 'https://edm-publishing.nyc3.digitaloceanspaces.com/db-developments/latest/output/'
         qc_jobtypestats = pd.read_csv(f'{u}qc_jobtypestats.csv', dtype=str)
         qc_countsstats = pd.read_csv(f'{u}qc_countsstats.csv', dtype=str)
         qc_units_complete_stats = pd.read_csv(f'{u}qc_units_complete_stats.csv', dtype=str)
         qc_geocodedstats = pd.read_csv(f'{u}qc_geocodedstats.csv', dtype=str)
-        qc_completeness = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-developments/latest/output/qc_completeness.csv')
-        qc_mismatch = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-developments/latest/output/qc_mismatch.csv')
-        qc_null = pd.read_csv('https://edm-publishing.nyc3.digitaloceanspaces.com/db-developments/latest/output/qc_null.csv')
+        qc_completeness = pd.read_csv(f'{u}qc_completeness.csv')
+        qc_mismatch = pd.read_csv(f'{u}qc_mismatch.csv')
+        qc_null = pd.read_csv(f'{u}qc_null.csv')
         return qc_jobtypestats, qc_countsstats, qc_units_complete_stats, qc_geocodedstats, qc_completeness, qc_mismatch, qc_null
 
     qc_jobtypestats, qc_countsstats, qc_units_complete_stats, qc_geocodedstats, qc_completeness, qc_mismatch, qc_null = get_data()
