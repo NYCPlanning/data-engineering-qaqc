e061b46b2f2284bc77cd6158827dff4cad9f401a
sptkl <caobaiyue@gmail.com>
2020-03-30 10:50:40 -0400

    adding qaqc tables for facdb

--- src/facdb.py
+++ src/facdb.py
@@ -11,71 +11,67 @@ def facdb():
     def get_data():
         url = 'https://edm-publishing.nyc3.digitaloceanspaces.com/db-facilities/latest/output'
         qc_diff = pd.read_csv(f'{url}/qc_diff.csv')
-        facdb = pd.read_csv(f'{url}/facilities.csv')
-        _facdb = pd.read_csv('https://edm-recipes.nyc3.digitaloceanspaces.com/random/facilities_201912.csv')
-        facdb['version'] = 'new'
-        _facdb['version'] = 'old'
-        facdb = facdb.round(4)
-        _facdb = _facdb.round(4)
-        combined = pd.concat([facdb, _facdb])
-        combined = combined.drop_duplicates(subset=['longitude', 'latitude'], keep=False)
-        return qc_diff, facdb, _facdb, combined
+        qc_captype = pd.read_csv(f'{url}/qc_captype.csv')
+        qc_capvalues = pd.read_csv(f'{url}/qc_capvalues.csv')
+        qc_classification = pd.read_csv(f'{url}/qc_classification.csv')
+        qc_mapped_datasource = pd.read_csv(f'{url}/qc_mapped_datasource.csv')
+        qc_mapped_subgroup = pd.read_csv(f'{url}/qc_mapped_subgroup.csv')
+        qc_operator = pd.read_csv(f'{url}/qc_operator.csv')
+        qc_oversight = pd.read_csv(f'{url}/qc_oversight.csv')
+        qc_proptype = pd.read_csv(f'{url}/qc_proptype.csv')
 
-    qc_diff, facdb, _facdb, combined = get_data()
-    datasource = st.selectbox('select a datasource', combined.datasource.unique())
-    st.dataframe(qc_diff.loc[(qc_diff.datasource == datasource)&(qc_diff.diff != 0), :])
-    
-    st.pydeck_chart(pdk.Deck(
-        map_style='mapbox://styles/mapbox/dark-v9',
-        initial_view_state=pdk.ViewState(
-            latitude=40.7128,
-            longitude=-74.0006,
-            zoom=11,
-        ),
-        layers=[
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=combined.loc[
-                        (combined.datasource == datasource)&
-                        (combined.version=='new'), 
-                        ['longitude', 'latitude','facname',
-                        'facdomain', 'facgroup', 'facsubgrp',
-                        'factype', 'datasource']]\
-                            .dropna(subset=['longitude', 'latitude']),
-                get_position='[longitude, latitude]',
-                get_color='[255, 0, 0, 160]',
-                get_radius=40,
-                pickable=True
-            ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=combined.loc[
-                        (combined.datasource == datasource)&
-                        (combined.version=='old'), 
-                        ['longitude', 'latitude','facname',
-                        'facdomain', 'facgroup', 'facsubgrp',
-                        'factype', 'datasource']]\
-                            .dropna(subset=['longitude', 'latitude']),
-                get_position='[longitude, latitude]',
-                get_color='[255, 255, 0, 160]',
-                get_radius=40,
-                pickable=True
-            )
-        ],
-        tooltip={
-            "html": '''
-                <div><b>facname: {facname} &nbsp;</b></div>
-                <ul>
-                <li>facdomain: {facdomain}</li>
-                <li>facgroup: {facgroup}</li>
-                <li>facsubgrp: {facsubgrp}</li>
-                <li>factype: {factype}</li>
-                <li>datasource: {datasource}</li>
-                </ul>
-            ''',
-            "style": {
-                    "backgroundColor": "steelblue",
-                    "color": "white"
+        def color_negative_red(val):
+            """
+            Takes a scalar and returns a string with
+            the css property `'color: red'` for negative
+            strings, black otherwise.
+            """
+            color = 'red' if val < 0 else 'black'
+            return 'color: %s' % color
+            
+        qc_tables = {
+                'Full Panel Cross Version Comparison':{
+                    'dataframe': qc_diff,
+                    'type': 'dataframe'
+                }, 
+                'Counts by Classification' : {
+                    'dataframe': qc_classification,
+                    'type': 'dataframe'
+                }, 
+                'Percentage Mapped by datasource' : {
+                    'dataframe': qc_mapped_datasource,
+                    'type': 'dataframe'
+                }, 
+                'Percentage Mapped by subgroup' : {
+                    'dataframe': qc_mapped_subgroup,
+                    'type': 'dataframe'
+                }, 
+                'Operator Counts' : {
+                    'dataframe': qc_operator,
+                    'type': 'dataframe'
+                }, 
+                'Oversight Counts' : {
+                    'dataframe': qc_oversight,
+                    'type': 'dataframe'
+                }, 
+                'Property Types' : {
+                    'dataframe': qc_proptype,
+                    'type': 'table'
+                }, 
+                'Capacity Types' : {
+                    'dataframe': qc_captype,
+                    'type': 'table'
+                }, 
             }
-        }
-    ))
+        return qc_tables
+
+    qc_tables = get_data()
+
+    for key, value in qc_tables.items():
+        st.header(key)
+        if value['type'] == 'dataframe':
+            st.dataframe(value['dataframe'])
+        else:
+            st.table(value['dataframe'])
+
+    
