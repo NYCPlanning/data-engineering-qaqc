d08f1c7db4d50bf5b175ee6d029c1c7b8bb73e42
sptkl <caobaiyue@gmail.com>
2021-03-29 14:14:44 +0000

    reorder and add text

--- src/ztl.py
+++ src/ztl.py
@@ -19,9 +19,7 @@ def ztl():
         qaqc_mismatch = pd.read_csv(f"{url}qaqc_mismatch.csv", index_col=False)
         bbldiff = pd.read_csv(f"{url}qc_bbldiffs.csv", dtype=str, index_col=False)
         bbldiff = bbldiff.fillna("NULL")
-        qaqc_null = pd.read_csv(
-            f"{url}qaqc_null.csv", index_col=False
-        )
+        qaqc_null = pd.read_csv(f"{url}qaqc_null.csv", index_col=False)
         last_build = requests.get(f"{url}version.txt").text
 
         return (
@@ -30,7 +28,7 @@ def ztl():
             last_build,
             qaqc_mismatch,
             qaqc_bbl,
-            qaqc_null
+            qaqc_null,
         )
 
     (
@@ -39,7 +37,7 @@ def ztl():
         last_build,
         qaqc_mismatch,
         qaqc_bbl,
-        qaqc_null
+        qaqc_null,
     ) = get_data()
 
     st.title("Zoning Tax Lots QAQC")
@@ -76,33 +74,50 @@ def ztl():
             "limitedheightdistrict",
         ],
     }
-    
+
     # TOTAL QAQC DIFF BY BOROUGH =============================
-    total_diff_by_borough = bbldiff.groupby(['boroughcode']).size().to_dict()
-    st.header('Total BBL Counts with Value Changes by Borough')
-    st.markdown(f'''
+    total_diff_by_borough = bbldiff.groupby(["boroughcode"]).size().to_dict()
+    st.header("Total BBL Counts with Value Changes by Borough")
+    st.markdown(
+        f"""
     Manhattan: **{total_diff_by_borough.get('1', '0')}**, Bronx: **{total_diff_by_borough.get('2', '0')}**, 
     Brooklyn: **{total_diff_by_borough.get('3', '0')}**, Queens: **{total_diff_by_borough.get('4', '0')}**,
     Staten Island: **{total_diff_by_borough.get('5', '0')}**
-    ''')
+    """
+    )
     # TOTAL QAQC DIFF BY BOROUGH =============================
 
     # Aggregated QAQC MISMATCH ===============================
-    total_mismatch = qaqc_mismatch[['version', 'version_prev']]
-    total_mismatch['total difference'] = qaqc_mismatch[[
-        "zoningdistrict1","zoningdistrict2","zoningdistrict3",
-        "zoningdistrict4", "commercialoverlay1", "commercialoverlay2",
-        "specialdistrict1", "specialdistrict2", "specialdistrict3",
-        "limitedheightdistrict", "zoningmapnumber", "zoningmapcode"
-    ]].sum(axis=1)
+    total_mismatch = qaqc_mismatch[["version", "version_prev"]]
+    total_mismatch["total difference"] = qaqc_mismatch[
+        [
+            "zoningdistrict1",
+            "zoningdistrict2",
+            "zoningdistrict3",
+            "zoningdistrict4",
+            "commercialoverlay1",
+            "commercialoverlay2",
+            "specialdistrict1",
+            "specialdistrict2",
+            "specialdistrict3",
+            "limitedheightdistrict",
+            "zoningmapnumber",
+            "zoningmapcode",
+        ]
+    ].sum(axis=1)
 
     st.header("Total BBL Pairwise Value Mismatch by Version")
     st.write(total_mismatch)
-    st.write('Total number of BBLs that have differing values between versions')
+    st.write("Total number of BBLs that have differing values between versions")
     # Aggregated QAQC MISMATCH ===============================
 
     # PLOTTING FUNCTION ======================================
-    def create_plot(df, group):
+    def create_plot(df, group, key):
+        # metric = st.radio(
+        #     "Pick which metric you would like to see for the plot",
+        #     ("raw counts", "raw counts - average"),
+        #     key=key,
+        # )
         fig = go.Figure()
         for i in group:
             fig.add_trace(
@@ -120,32 +135,44 @@ def ztl():
             yaxis=dict(title="Difference"),
         )
         st.plotly_chart(fig)
-    # PLOTTING FUNCTION ======================================  
+
+    # PLOTTING FUNCTION ======================================
 
     # LINEGRAPH BY EACH BOUNDARY TYPE ========================
     st.header("BBL Pairwise Value Mismatch by Field Category")
-    change_by_category = qaqc_mismatch[['version', 'version_prev']]
-    category = ['Commercial Overlay', 'Zoning Districts', 'Special Districts', 'Other']
+    change_by_category = qaqc_mismatch[["version", "version_prev"]]
+    category = ["Commercial Overlay", "Zoning Districts", "Special Districts", "Other"]
     for cat in category:
         change_by_category[cat] = qaqc_mismatch[meta[cat]].sum(axis=1)
-    create_plot(change_by_category, category)
+    create_plot(change_by_category, category, key="cat")
+    st.markdown(
+        """
+    This plot shows the total number of bbls with a value mismatch for different zoning field categories
+    """
+    )
     # LINEGRAPH BY EACH BOUNDARY TYPE ========================
-
-    # BBL ADDED / REMOVED ====================================
-    st.header("BBLs added/removed")
-    create_plot(qaqc_bbl, ["added", "removed"])
-    # BBL ADDED / REMOVED ====================================
 
     # Value to NULL Comparison ===============================
     st.header("BBL Pairwise NULL to Value and Value to NULL Counts")
-    st.write(qaqc_null[['field', 'value_to_null', 'null_to_value']])
+    st.write(qaqc_null[["field", "value_to_null", "null_to_value"]])
     st.markdown(
-    """
+        """
     - `value_to_null`: for given field, count of records with values in previous version and NULL in current version
     - `null_to_value`: for given field, count of records with NULL in previous version and values in current version
     """
     )
     # Value to NULL Comparison ===============================
+
+    # BBL ADDED / REMOVED ====================================
+    st.header("BBLs added/removed")
+    create_plot(qaqc_bbl, ["added", "removed"], key="bbl")
+    st.markdown(
+        """
+    - Number of new bbl added (in current version but not in previous version) 
+    - Number of old bbl removed (in previous version but not in current version)
+    """
+    )
+    # BBL ADDED / REMOVED ====================================
 
     # SOURCE DATA REPORT  ====================================
     st.header("Source Data Versions")
