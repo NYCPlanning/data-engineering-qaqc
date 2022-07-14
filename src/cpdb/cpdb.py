be95f6a571ef34755f14ca1041ce975c8f729d73
Te Du <tedudevelop@gmail.com>
2022-07-14 14:23:40 -0400

    first draft for visualize summary tables

--- src/cpdb/cpdb.py
+++ src/cpdb/cpdb.py
@@ -1,6 +1,7 @@
 import streamlit as st  # type: ignore
 import pandas as pd
 from src.cpdb.helpers import get_data
+import plotly.express as px
 
 def cpdb():
     st.title("Capital Projects DB QAQC")
@@ -11,7 +12,31 @@ def cpdb():
     data = get_data(branch)
 
     st.header('Managing Agency Summary Stats')
+    
     st.dataframe(data['magency'])
+
+    # getting the 
+    st.subheader('Top Ten Managing Agencies By Project Count or Commitment')
+    sorter = st.selectbox('Sort Agencies Based On',
+    options=('Total Projects', 'Total Commitment Amount'), index=0)
+    if sorter == 'Total Projects':
+        st.plotly_chart(
+            px.bar(data['magency'].sort_values(by='totalcount', ascending=False).head(10), 
+                x='magencyacro', 
+                y=['totalcount', 'mapped'],
+                barmode='group'
+            )
+        )
+    else: 
+        st.plotly_chart(
+            px.bar(data['magency'].sort_values(by='totalcommit', ascending=False).head(10), 
+                x='magencyacro', 
+                y=['totalcommit', 'mappedcommit'],
+                barmode='group'
+            )
+        )
+
+    
 
     st.header("Sponsoring Agency Summary Stats")
     st.dataframe(data['sagency'])
