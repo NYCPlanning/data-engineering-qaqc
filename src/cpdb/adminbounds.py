f7610c1f72664bdbabd57ec89892ebed3d43bf46
Te Du <tedudevelop@gmail.com>
2022-08-02 11:51:10 -0400

    set the output if it is a list

--- src/cpdb/adminbounds.py
+++ src/cpdb/adminbounds.py
@@ -1,9 +1,10 @@
 def adminbounds(df, pre_df):
-    output = list(set(pre_df.admin_boundary_type.unique()) - set(df.admin_boundary_type.unique()))
+    output = list(set(pre_df.admin_boundary_type.unique()) -
+                  set(df.admin_boundary_type.unique()))
 
     if len(output) == 0:
         output = "No change"
     else:
-        ",".join(output)
+        output = ",".join(output)
 
     return output
