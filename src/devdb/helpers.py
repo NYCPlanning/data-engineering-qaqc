8110fc9059cd459c5f9cfcac6ecc1b2baf4a3c2e
Andrew Brieff <abreff@gmail.com>
2022-08-04 15:45:18 -0400

    remove extra imports, set datatype

--- src/devdb/helpers.py
+++ src/devdb/helpers.py
@@ -1,23 +1,20 @@
-import boto3
-import re
 import pandas as pd
-from datetime import datetime
-import json
 from typing import Dict
-import os
 from dotenv import load_dotenv
-from zipfile import ZipFile
-from io import BytesIO
+
 
 load_dotenv()
 
 BUCKET_NAME = "edm-publishing"
 
 
-def get_data() -> Dict[str, pd.DataFrame]:
+def get_data():
     rv = {}
     url = f"https://edm-publishing.nyc3.digitaloceanspaces.com/db-developments/latest/output"
-    rv["qaqc_app"] = csv_from_DO(f"{url}/qaqc_app.csv")
+
+    rv["qaqc_app"] = csv_from_DO(
+        f"{url}/qaqc_app.csv", kwargs={"dtype": {"job_number": "str"}}
+    )
 
     return rv
 
