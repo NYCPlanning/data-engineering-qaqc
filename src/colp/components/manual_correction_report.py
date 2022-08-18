6971e938710862948c01483275d5cb4337715bb9
Jingyi Huang <jh5721@nyu.edu>
2022-08-18 15:47:53 +0000

    handling missing data

--- src/colp/components/manual_correction_report.py
+++ src/colp/components/manual_correction_report.py
@@ -16,11 +16,16 @@ class ManualCorrection:
         ):
             st.info("No Manual Correction Table Available.")
             return
-        self.display_manual_correction()
+        elif self.modifications_applied is None:
+            self.display_not_applied_correction()
+        elif self.modifications_not_applied is None:
+            self.display_applied_correction()
+        else:
+            self.display_applied_correction()
+            self.display_not_applied_correction()
 
-    def display_manual_correction(self):
+    def display_applied_correction(self):
         applied = self.modifications_applied
-        not_applied = self.modifications_not_applied
         if not applied.empty:
             count1 = applied.groupby("field").size()
             count1 = count1.reset_index()
@@ -29,6 +34,9 @@ class ManualCorrection:
             )
             st.subheader("Numbers of Manual Corrections Applied By Field")
             st.write(count1)
+
+    def display_not_applied_correction(self):
+        not_applied = self.modifications_not_applied
         if not not_applied.empty:
             count2 = not_applied.groupby("field").size()
             count2 = count2.reset_index()
@@ -38,5 +46,6 @@ class ManualCorrection:
             st.subheader("Numbers of Manual Corrections Not Applied By Field")
             st.write(count2)
             not_applied = not_applied.sort_values(by=["field"])
+            not_applied = not_applied[not_applied.columns.tolist()[1:] + ["uid"]]
             st.subheader("Table of Manual Corrections Not Applied")
             st.write(not_applied)
