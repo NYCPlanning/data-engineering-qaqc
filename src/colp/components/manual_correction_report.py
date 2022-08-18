7d37d398a3db58d90743086dc320b0c9c6cb5b0a
Jingyi Huang <jh5721@nyu.edu>
2022-08-18 13:34:37 +0000

    fix issues

--- src/colp/components/manual_correction_report.py
+++ src/colp/components/manual_correction_report.py
@@ -14,7 +14,7 @@ class ManualCorrection:
             self.modifications_applied is None
             and self.modifications_not_applied is None
         ):
-            st.info("No Manual Correction Table.")
+            st.info("No Manual Correction Table Available.")
             return
         self.display_manual_correction()
 
@@ -23,16 +23,20 @@ class ManualCorrection:
         not_applied = self.modifications_not_applied
         if not applied.empty:
             count1 = applied.groupby("field").size()
-            count1.rename("Number of Records", inplace=True)
-            st.markdown("##### Numbers of Manual Corrections Applied By Field")
+            count1 = count1.reset_index()
+            count1.rename(
+                columns={"field": "Field", 0: "Number of Records"}, inplace=True
+            )
+            st.subheader("Numbers of Manual Corrections Applied By Field")
             st.write(count1)
         if not not_applied.empty:
             count2 = not_applied.groupby("field").size()
-            count2.rename("Number of Records", inplace=True)
-            st.markdown("##### Numbers of Manual Corrections Not Applied By Field")
+            count2 = count2.reset_index()
+            count2.rename(
+                columns={"field": "Field", 0: "Number of Records"}, inplace=True
+            )
+            st.subheader("Numbers of Manual Corrections Not Applied By Field")
             st.write(count2)
-            uid = not_applied["uid"]
-            not_applied = not_applied.drop(columns=["uid"]).sort_values(by=["field"])
-            not_applied = pd.concat([not_applied, uid], axis=1)
-            st.markdown("##### Table of Manual Corrections Not Applied")
+            not_applied = not_applied.sort_values(by=["field"])
+            st.subheader("Table of Manual Corrections Not Applied")
             st.write(not_applied)
