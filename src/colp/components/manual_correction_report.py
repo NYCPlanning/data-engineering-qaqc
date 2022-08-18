ae784c750c27b8c6bf34a05b6f049e13d78d3ba7
Jingyi Huang <jh5721@nyu.edu>
2022-08-18 19:38:28 +0000

    fix

--- src/colp/components/manual_correction_report.py
+++ src/colp/components/manual_correction_report.py
@@ -18,11 +18,16 @@ class ManualCorrection:
             st.write("Manual correction table for applied records is not available.")
             return
         applied = self.modifications_applied
-        count1 = applied.groupby("field").size()
-        count1 = count1.reset_index()
-        count1.rename(columns={"field": "Field", 0: "Number of Records"}, inplace=True)
-        st.subheader("Numbers of Manual Corrections Applied By Field")
-        st.write(count1)
+        if not applied.empty:
+            count1 = applied.groupby("field").size()
+            count1 = count1.reset_index()
+            count1.rename(
+                columns={"field": "Field", 0: "Number of Records"}, inplace=True
+            )
+            st.subheader("Numbers of Manual Corrections Applied By Field")
+            st.write(count1)
+        else:
+            st.write("Manual correction table for applied records is empty.")
 
     def display_not_applied_correction(self, df):
         if df is None:
@@ -31,12 +36,17 @@ class ManualCorrection:
             )
             return
         not_applied = self.modifications_not_applied
-        count2 = not_applied.groupby("field").size()
-        count2 = count2.reset_index()
-        count2.rename(columns={"field": "Field", 0: "Number of Records"}, inplace=True)
-        st.subheader("Numbers of Manual Corrections Not Applied By Field")
-        st.write(count2)
-        not_applied = not_applied.sort_values(by=["field"])
-        not_applied = not_applied[not_applied.columns.tolist()[1:] + ["uid"]]
-        st.subheader("Table of Manual Corrections Not Applied")
-        AgGrid(not_applied)
+        if not not_applied.empty:
+            count2 = not_applied.groupby("field").size()
+            count2 = count2.reset_index()
+            count2.rename(
+                columns={"field": "Field", 0: "Number of Records"}, inplace=True
+            )
+            st.subheader("Numbers of Manual Corrections Not Applied By Field")
+            st.write(count2)
+            not_applied = not_applied.sort_values(by=["field"])
+            not_applied = not_applied[not_applied.columns.tolist()[1:] + ["uid"]]
+            st.subheader("Table of Manual Corrections Not Applied")
+            AgGrid(not_applied)
+        else:
+            st.write("Manual correction table for not applied records is empty.")
