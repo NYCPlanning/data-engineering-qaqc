5eeeb608f12c334f8a96699f2d49faa237357079
Jingyi Huang <jh5721@nyu.edu>
2022-08-18 19:02:33 +0000

    modify

--- src/colp/components/manual_correction_report.py
+++ src/colp/components/manual_correction_report.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from st_aggrid import AgGrid
 
 
 class ManualCorrection:
@@ -9,43 +10,33 @@ class ManualCorrection:
 
     def __call__(self):
         st.subheader("Manual Correction Report")
+        self.display_applied_correction(self.modifications_applied)
+        self.display_not_applied_correction(self.modifications_not_applied)
 
-        if (
-            self.modifications_applied is None
-            and self.modifications_not_applied is None
-        ):
-            st.info("No Manual Correction Table Available.")
+    def display_applied_correction(self, df):
+        if df is None:
+            st.write("Manual correction table for applied records is not available.")
             return
-        elif self.modifications_applied is None:
-            self.display_not_applied_correction()
-        elif self.modifications_not_applied is None:
-            self.display_applied_correction()
-        else:
-            self.display_applied_correction()
-            self.display_not_applied_correction()
-
-    def display_applied_correction(self):
         applied = self.modifications_applied
-        if not applied.empty:
-            count1 = applied.groupby("field").size()
-            count1 = count1.reset_index()
-            count1.rename(
-                columns={"field": "Field", 0: "Number of Records"}, inplace=True
-            )
-            st.subheader("Numbers of Manual Corrections Applied By Field")
-            st.write(count1)
+        count1 = applied.groupby("field").size()
+        count1 = count1.reset_index()
+        count1.rename(columns={"field": "Field", 0: "Number of Records"}, inplace=True)
+        st.subheader("Numbers of Manual Corrections Applied By Field")
+        st.write(count1)
 
-    def display_not_applied_correction(self):
-        not_applied = self.modifications_not_applied
-        if not not_applied.empty:
-            count2 = not_applied.groupby("field").size()
-            count2 = count2.reset_index()
-            count2.rename(
-                columns={"field": "Field", 0: "Number of Records"}, inplace=True
+    def display_not_applied_correction(self, df):
+        if df is None:
+            st.write(
+                "Manual correction table for not applied records is not available."
             )
-            st.subheader("Numbers of Manual Corrections Not Applied By Field")
-            st.write(count2)
-            not_applied = not_applied.sort_values(by=["field"])
-            not_applied = not_applied[not_applied.columns.tolist()[1:] + ["uid"]]
-            st.subheader("Table of Manual Corrections Not Applied")
-            st.write(not_applied)
+            return
+        not_applied = self.modifications_not_applied
+        count2 = not_applied.groupby("field").size()
+        count2 = count2.reset_index()
+        count2.rename(columns={"field": "Field", 0: "Number of Records"}, inplace=True)
+        st.subheader("Numbers of Manual Corrections Not Applied By Field")
+        st.write(count2)
+        not_applied = not_applied.sort_values(by=["field"])
+        not_applied = not_applied[not_applied.columns.tolist()[1:] + ["uid"]]
+        st.subheader("Table of Manual Corrections Not Applied")
+        AgGrid(not_applied)
