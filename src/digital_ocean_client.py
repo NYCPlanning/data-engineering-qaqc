529ae4aaf4a18121d9ad99c622ff62e72d180d2c
Andrew Brieff <abreff@gmail.com>
2022-08-18 08:19:18 -0400

    Make code clearer, save s3_resource so we dont keep reinstating it

--- src/digital_ocean_client.py
+++ src/digital_ocean_client.py
@@ -5,7 +5,6 @@ from zipfile import ZipFile
 from io import StringIO
 import os
 from dotenv import load_dotenv
-import pdb
 import streamlit as st
 from datetime import datetime
 import shutil
@@ -18,9 +17,13 @@ class DigitalOceanClient:
     def __init__(self, bucket_name, repo_name):
         self.bucket_name = bucket_name
         self.repo_name = repo_name
+        self.s3_resource = self.get_s3_resource()
 
     def bucket(self):
-        return self.s3_resource().Bucket(self.bucket_name)
+        return self.s3_resource.Bucket(self.bucket_name)
+
+    def bucket_is_public(self):
+        return self.bucket_name == "edm-publishing"
 
     def get_folders(self):
         return self.bucket().objects.filter(Prefix=f"{self.repo_name}/")
@@ -43,7 +46,7 @@ class DigitalOceanClient:
         except:
             return None
 
-    def s3_resource(self):
+    def get_s3_resource(self):
         return boto3.resource(
             "s3",
             aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
@@ -53,22 +56,25 @@ class DigitalOceanClient:
 
     def csv_from_DO(self, url, kwargs={}):
         try:
-            if self.bucket_name == "edm-publishing":
-                return pd.read_csv(url, **kwargs)
+            if self.bucket_is_public():
+                return self.public_csv_from_DO(url, kwargs)
             else:
                 return self.private_csv_from_DO(url, kwargs)
         except:
             return None
 
+    def public_csv_from_DO(self, url, kwargs):
+        return pd.read_csv(url, **kwargs)
+
     def private_csv_from_DO(self, url, kwargs):
-        obj = self.s3_resource().Object(bucket_name=self.bucket_name, key=url)
+        obj = self.s3_resource.Object(bucket_name=self.bucket_name, key=url)
         s = str(obj.get()["Body"].read(), "utf8")
         data = StringIO(s)
 
         return pd.read_csv(data, encoding="utf8", **kwargs)
 
     def zip_from_DO(self, zip_filename):
-        zip_obj = self.s3_resource().Object(
+        zip_obj = self.s3_resource.Object(
             bucket_name=self.bucket_name, key=zip_filename
         )
         buffer = BytesIO(zip_obj.get()["Body"].read())
