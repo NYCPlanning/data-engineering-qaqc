3decab5b04dab1593adbf361048e2bc695f1ab22
Damon McCullough <damonmcc1391@gmail.com>
2023-04-11 15:11:15 +0000

    dev env stuff

--- bash/install_linux_packages.sh
+++ bash/install_linux_packages.sh
@@ -3,6 +3,4 @@
 # Installs linux packages required for building and testing.
 set -e
 
-# Install postgresql client to use psql (postgresql alias)
 apt-get update
-apt-get --assume-yes install --no-install-recommends postgresql-client gdal-bin
