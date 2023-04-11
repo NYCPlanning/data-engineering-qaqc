22d89a9f32ef1fd13501aefb9c4ea2c4c09f220b
Damon McCullough <damonmcc1391@gmail.com>
2023-04-11 16:32:22 +0000

    more db use

--- src/ztl/components/inputs_report.py
+++ src/ztl/components/inputs_report.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.ztl.helpers import REFERENCE_VESION, get_source_data_versions, get_source_data
+from src.ztl.helpers import REFERENCE_VESION, get_source_data_versions, load_source_data
 
 
 def inputs_report():
@@ -8,23 +8,49 @@ def inputs_report():
         f"""
     This page reviews the status of all source data used to build this dataset.
 
-    For the purposes of this review:
-    - the reference version is `{REFERENCE_VESION}`
+    - the reference dataset version is `{REFERENCE_VESION}`
     - ...
     """
     )
 
-    st.subheader("Source Data Versions")
-    reference_source_data_versions = get_source_data_versions(REFERENCE_VESION)
-    latests_source_data_versions = get_source_data_versions()
+    st.subheader("Compare Source Data Versions")
+    reference_source_data_versions = get_source_data_versions(version=REFERENCE_VESION)
+    latest_source_data_versions = get_source_data_versions(version="latest")
     source_data_versions = reference_source_data_versions.merge(
-        latests_source_data_versions,
+        latest_source_data_versions,
         on="datalibrary_name",
-        suffixes=('_reference', '_latest'),
+        suffixes=("_reference", "_latest"),
     )
     st.table(source_data_versions)
 
-    st.subheader("Source Data Schemas")
-    ex_source_data = get_source_data("dcp_zoningmapamendments")
+    if not st.session_state.get("source_load_button", False):
+        st.session_state.data_loaded = False
+        st.button(
+            label="⬇️ Load source data",
+            use_container_width=True,
+            key="source_load_button",
+        )
+        return
+    st.session_state.data_loaded = True
+    st.button(
+        label="🔄 Refrash page to reload source data",
+        use_container_width=True,
+        key="source_load_button",
+        disabled=True,
+    )
 
-    st.subheader("Source Data Row Counts")
+    # TODO add details about tables in ZTL QAQC postgress DB
+
+    st.subheader("Compare Source Data Schemas")
+    # TODO load all source datasets into a DB
+    print("LOADING ALL SOURCE DATA")
+    load_source_data(
+        dataset="dcp_zoningmapamendments",
+        version=REFERENCE_VESION,
+    )
+    load_source_data(
+        dataset="dcp_zoningmapamendments",
+        version="latest",
+    )
+
+    st.subheader("Compare Source Data Row Counts")
