f73cb7214b6371148b518bf8720c0d94e641b0e7
Damon McCullough <damonmcc1391@gmail.com>
2023-04-13 15:52:06 +0000

    more tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,11 +1,38 @@
 # test s3 and sql data IO
+from src.postgres_utils import get_table_columns
 from src.digital_ocean_utils import get_datatset_config
+from src.ztl.helpers import DATASET_QAQC_DB_SCHEMA
 
+TEST_DATASET_NAME = DATASET_QAQC_DB_SCHEMA
 TEST_DATA_SOURCE_NAME = "dcp_zoningmapamendments"
 TEST_DATA_SOURCE_VERSION = "20230404"
+TEST_DATA_SOURCE_COLUMNS = [
+    "effective",
+    "lucats",
+    "ogc_fid",
+    "project_na",
+    "shape_area",
+    "shape_leng",
+    "status",
+    "ulurpno",
+    "wkb_geometry",
+]
+
 
 def test_dataset_config():
-    dataset_confg = get_datatset_config(dataset=TEST_DATA_SOURCE_NAME, version=TEST_DATA_SOURCE_VERSION)
+    dataset_confg = get_datatset_config(
+        dataset=TEST_DATA_SOURCE_NAME, version=TEST_DATA_SOURCE_VERSION
+    )
     assert isinstance(dataset_confg, dict)
     assert dataset_confg["dataset"]["name"] == TEST_DATA_SOURCE_NAME
     assert dataset_confg["dataset"]["version"] == TEST_DATA_SOURCE_VERSION
+
+
+def test_get_table_columns():
+    columns = get_table_columns(
+        # table_schema="db_zoningtaxlots",
+        # table_name="dcp_zoningmapamendments_20230306",
+        table_schema=TEST_DATASET_NAME,
+        table_name=f"{TEST_DATA_SOURCE_NAME}_{TEST_DATA_SOURCE_VERSION}",
+    )
+    assert columns == TEST_DATA_SOURCE_COLUMNS
--- tests/test_source_report.py
+++ tests/test_source_report.py
@@ -1,10 +1,26 @@
+# TODO abstract these functions out of ztl directory
 import pandas as pd
-from src.ztl.helpers import get_latest_source_data_versions
+from src.ztl.helpers import get_latest_source_data_versions, get_source_dataset_names
 
 TEST_DATA_SOURCE_NAME = "dcp_zoningmapamendments"
 TEST_DATA_SOURCE_VERSION = "20230404"
+TEST_DATA_SOURCE_NAMES = [
+    "dcp_commercialoverlay",
+    "dcp_limitedheight",
+    "dcp_specialpurpose",
+    "dcp_specialpurposesubdistricts",
+    "dcp_zoningdistricts",
+    "dcp_zoningmapamendments",
+    "dcp_zoningmapindex",
+    "dof_dtm",
+]
 
 def test_get_latest_source_data_versions():
     source_data_versions = get_latest_source_data_versions()
     assert isinstance(source_data_versions, pd.DataFrame)
     assert source_data_versions.loc[TEST_DATA_SOURCE_NAME, "version"] == TEST_DATA_SOURCE_VERSION
+
+
+def test_get_source_dataset_names():
+    source_dataset_names = get_source_dataset_names()
+    assert source_dataset_names == TEST_DATA_SOURCE_NAMES
