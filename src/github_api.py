1ec025ac0515e3bd513e4796be46cf86c48d200e
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-04-20 15:31:19 +0000

    chk, remove token

--- src/github_api.py
+++ src/github_api.py
@@ -1,26 +1,12 @@
 import os
-from github import Github
 import requests
-import json
-import pandas as pd
-import matplotlib.pyplot as plt
-from datetime import datetime, date
+import streamlit as st
 
 ORG = 'NYCPlanning'
-PERSONAL_TOKEN = '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' # Your app token
+PERSONAL_TOKEN = os.environ['ghp_token'] # Your app token
 headers = {'Authorization': 'Bearer %s' % PERSONAL_TOKEN}
 params = {'filter': 'all', 'per_page': 100}
 BASE_URL=f'https://api.github.com/repos/{ORG}'
-
-action_types = [
-    'address-points-vs-pad',
-    'addresses-spatial',
-    'footprints-vs-pad',
-    'housing',
-    'pad-vs-footprint',
-    'dcm-streetname',
-    'saf-vs-pad',
-]
 
 def parse_workflow(workflow):
     return {
@@ -29,27 +15,31 @@ def parse_workflow(workflow):
         'timestamp': workflow['updated_at'],
     }
 
-def get_workflow(repo, name): ## unnecessary since we can just use the name to get the runs
+def get_workflow(repo, name): 
     url = f"{BASE_URL}/{repo}/actions/workflows/{name}"
     r = requests.get(url, headers=headers)
     return r.json()
 
-def get_workflow_runs(repo, workflow_name = None): # /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs
-    #if workflow_name: 
-    url = f"{BASE_URL}/{repo}/actions/workflows/{workflow_name}/runs"
-    #else:
-    #    url = f"{BASE_URL}/{repo}/actions/runs"
-    workflows = {}
-    for run in requests.get(url, headers=headers, params=params).json()['workflow_runs']:
-        if len(workflows) == 7: break
-        if '[' in run['display_title']: ## runs triggered by issues
-            name = run['display_title'].split('[', 1)[1].split(']')[0]
-        else: ## runs triggered by this app
-            name = run['name']
-        if name in action_types:
-            if name not in workflows:
-                workflows.update(name, parse_workflow(run))
-    return workflows
+def get_workflow_runs(repo, workflow_name = None, items_per_page = None, total_items = None):
+    if workflow_name: 
+        url = f"{BASE_URL}/{repo}/actions/workflows/{workflow_name}/runs"
+    else:
+        url = f"{BASE_URL}/{repo}/actions/runs"
+    if items_per_page is None and total_items is None:
+        return requests.get(url, headers=headers).json()['workflow_runs']
+    elif items_per_page is None:
+        return requests.get(url, headers=headers, params={'per_page': items_per_page}).json()['workflow_runs']
+    else:
+        workflows = []
+        page = 0
+        res = []
+        while (total_items is None and (page == 0 or len(res) != 0)) or (total_items is not None and len(workflows) < total_items):
+            page += 1
+            res = requests.get(url, headers=headers, params={'items_per_page': items_per_page, 'page': page}).json()['workflow_runs']
+            workflows = workflows.append(res)
+            if total_items is not None and len(workflows) < total_items: 
+                workflows = workflows[:total_items]
+        return workflows
 
 def dispatch_workflow(repo, workflow_name, **inputs):
     params = {'ref': 'main', 'inputs': inputs}
@@ -57,3 +47,6 @@ def dispatch_workflow(repo, workflow_name, **inputs):
     response = requests.post(url, headers=headers, params=params)
     if not response.json()['Status'] == '204':
         raise "aaaagh"
+
+def dispatch_workflow_button(repo, workflow_name, label='Run', disabled=False, **inputs):
+    return st.button(label, on_click=dispatch_workflow(repo, workflow_name, **inputs), disabled=disabled)
