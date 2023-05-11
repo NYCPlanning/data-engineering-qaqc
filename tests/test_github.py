762ceb0441a79390ac9e0be24d2efe2464e3281f
Damon McCullough <damonmcc1391@gmail.com>
2023-05-11 14:37:03 +0000

    test get_branches

--- tests/test_github.py
+++ tests/test_github.py
@@ -3,5 +3,5 @@ from src.github import get_branches
 TEST_REPO = "data-engineering-qaqc"
 
 def test_get_branches():
-    branches = get_branches(repo=TEST_REPO, branches_blacklist=[])
+    branches = get_branches(repo=TEST_REPO)
     assert "master" in branches
