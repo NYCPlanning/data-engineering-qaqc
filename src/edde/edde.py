732c7bcf249beb11ab4e439678c109e6ee53a27d
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-05-17 18:07:34 +0000

    working summaries of column header changes for all categories

--- src/edde/edde.py
+++ src/edde/edde.py
@@ -1,55 +1,24 @@
-import streamlit as st  # type: ignore
-import pandas as pd
-import numpy as np
-import requests
+import streamlit as st
+
 from src.constants import COLOR_SCHEME
 from src.digital_ocean_utils import DigitalOceanClient
-from helpers import get_data, compare_columns
+from src.report_utils import get_active_s3_folders
+from src.edde.helpers import (
+    demographic_categories,
+    other_categories,
+    geographies,
+    get_demographics_data,
+    get_other_data,
+)
+from src.edde.components import column_comparison_table
 
-categories = [
-    "demographics",
-    "economics",
-    "housing_prodution",
-    "housing_security",
-    "quality_of_life"
-]
-
-geographies = [
-    "citywide",
-    "borough",
-    "puma"
-]
-
-def get_default_branch(repo:str):
-    url = f"https://api.github.com/repos/nycplanning/{repo}"
-    response = requests.get(url).json()
-    return response["default_branch"]
-
-
-def get_branches(repo:str, branches_blacklist:list):
-    url = f"https://api.github.com/repos/nycplanning/{repo}/branches"
-    response = requests.get(url).json()
-    all_branches = [branch_info["name"] for branch_info in response]
-    return [
-        b for b in all_branches if b not in branches_blacklist
-    ]
-
-def get_active_s3_folders(repo:str, bucket_name:str):
-    default_branch = get_default_branch(repo=repo)
-    all_branches = get_branches(repo=repo, branches_blacklist=[])
-    all_folders = DigitalOceanClient(
-        bucket_name=bucket_name, repo_name=repo,
-    ).get_all_folder_names_in_repo_folder()
-
-    folders = sorted(list(set(all_folders).intersection(set(all_branches))))
-    folders.remove(default_branch)
-    folders = [default_branch] + folders
-    return folders
 
 def edde():
     st.title("EDDE QAQC")
 
-    branches = get_active_s3_folders(repo="db-equitable-development-tool", bucket_name="edm-publishing")
+    branches = get_active_s3_folders(
+        repo="db-equitable-development-tool", bucket_name="edm-publishing"
+    )
     branch = st.sidebar.selectbox(
         "Select a branch (will use latest)",
         branches,
@@ -57,51 +26,65 @@ def edde():
     )
 
     branch_comp = st.sidebar.selectbox(
-        "select a branch for comparison",
+        "Select a branch for comparison",
         branches,
         index=branches.index("main"),
     )
 
     date_comp = st.sidebar.selectbox(
-        "select a branch for comparison",
-        [], ##todo - all other than latest if same branch, or latest if other branch
-    )
-    if st.sidebar.button(
-        label="Refresh data", help="Download newest files from Digital Ocean"
-    ):
-
-        st.cache_data.clear()
-        get_data(branch)
-
-    old_data = get_data(branch_comp, date_comp)
-    new_data = get_data(branch, "latest")
-
-    try_pairing = st.sidebar.checkbox(
-        label="Try to match altered columns",
-        help="Some datasets have columns which remain effectively the same but update year in header.\nThis will try to identify those"
+        "Select an export for comparison",
+        DigitalOceanClient(
+            bucket_name="edm-publishing",
+            repo_name=f"db-eddt/{branch_comp}",
+        ).get_all_folder_names_in_repo_folder(
+            index=2
+        ),  ##todo - all other than latest if same branch, or latest if other branch
     )
 
-    for geography in geographies:
-        for category in category:
-            st.header(f"{category} - {geography}")
-            old = old_data[category][geography]
-            new = new_data[category][geography]
+    category_type = st.sidebar.selectbox(
+        "Select category type", ["Demographics", "Housing/Quality of Life"]
+    )
 
-            lost, added, union, paired = compare_columns(old, new, try_pairing)
-            nrows_mismatch = max(len(lost), len(added))
+    if category_type == "Demographics":
+        comparison_type = st.sidebar.selectbox(
+            "Comparison Type",
+            ["Between versions", "Between ACS years"],
+            help="Each build, exports are made for each ACS year. Comparisons can be made between same export files (ACS year) for different builds OR between different ACS years in the current build",
+        )
 
-            st.write(f"{len(union)} matching columns")
-            col1, col2 = st.columns(2)
-            col1.write("Lost")
-            col2.write("Added")
-            
-            for (old_column, matched_columns) in paired:
-                with col1: st.warning(old_column)
-                with col2: st.warning(", ".join(matched_columns))
+        old_data = get_demographics_data(branch_comp, date_comp)
+        new_data = get_demographics_data(branch, "latest")
 
-            for i in range(nrows_mismatch):
-                with col1: st.error(lost[i])
-                with col2: st.success(added[i])
+        for category in demographic_categories:
+            for geography in geographies:
+                old = old_data[category][geography]
+                new = new_data[category][geography]
+                keys = list(new.keys())
+                keys.sort(key=lambda x: x[-2:])
+                if comparison_type == "Between versions":
+                    for year in keys:
+                        st.header(f"{category}_{year}_{geography}.csv")
+                        if year in old:
+                            column_comparison_table(old[year], new[year])
+                        else:
+                            st.write("File not present in comparison build")
 
+                elif comparison_type == "Between ACS years":
+                    for i in range(len(keys) - 1):
+                        old_acs_year = keys[i]
+                        new_acs_year = keys[i + 1]
+                        st.header(f"{category} {geography}: {old_acs_year} vs {new_acs_year}")
+                        column_comparison_table(new[old_acs_year], new[new_acs_year])
+
+    elif category_type == "Housing/Quality of Life":
+        old_data = get_other_data(branch_comp, date_comp)
+        new_data = get_other_data(branch, "latest")
+
+        for category in other_categories:
+            for geography in geographies:
+                st.header(f"{category}_{geography}.csv")
+                old = old_data[category][geography]
+                new = new_data[category][geography]
+                column_comparison_table(old, new)
 
     ## Columns - dropped or added from last version
