735b029a83d0e0c59f4ac2a3ccf4e2194e2b1ac2
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 13:41:57 -0400

    typo

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -2,8 +2,8 @@
 
 apk add git-subtree
 /bin/setup-ssh
-git config --global --add safe.directory /__w/data-engineering-qaqc/data-engineering-qaqc
+git config --global --add safe.directory "$PWD"
 git subtree split -P test -b deploy
-git checkout deploy
-git push "$GIT_REMOTE_URL" "$deploy:refs/heads/main" --force 
-ssh -p "$SSH_PORT" "dokku@$DOKKU_HOST" apps:unlock "$APP_NAME"
+git push "$GIT_REMOTE_URL" "deploy:refs/heads/main" --force 
+
+dokku-unlock
