20765766caeba96d502ea83bf565758e6bb53e9b
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 15:25:49 -0400

    revert to not using dokku action

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 mv $APP_DIR/Dockerfile ./Dockerfile
+
+/bin/setup-ssh
+
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add .
 git commit -m "deployment commit"
+
+git push "$GIT_REMOTE_URL" "HEAD:refs/heads/$APP_REMOTE_BRANCH" --force 
