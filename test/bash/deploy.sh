da57b9b39fafb9b959f8d40058788a57108f3d40
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 14:21:45 -0400

    dokku branch master

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -3,15 +3,8 @@
 apk add git-subtree
 /bin/setup-ssh
 
-echo "github_sha: $GITHUB_SHA"
-echo "rev parse pre-checkout: $(git rev-parse HEAD 2>/dev/null || true)"
-
 git config --global --add safe.directory "$PWD"
 git subtree split -P test -b deploy
 git checkout deploy
 
-echo "rev parse post-checkout: $(git rev-parse HEAD 2>/dev/null || true)"
-
-git push "$GIT_REMOTE_URL" "deploy:refs/heads/main" --force 
-
-dokku-unlock
+git push "$GIT_REMOTE_URL" "deploy:refs/heads/master" --force 
