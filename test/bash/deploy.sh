4c6920496de4ed785e7310600e015680cbb247be
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 10:32:45 -0400

    ssh key path

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -2,18 +2,18 @@
 set -e
 
 mkdir -p .ssh
-echo "$ssh_private_key" | tr -d '\r' > .ssh/id_rsa
-chmod 600 .ssh/id_rsa
-chmod 700 .ssh
+echo "$ssh_private_key" | tr -d '\r' > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+chmod 700 ~/.ssh
 
 ssh_port=22
 
 echo "Generating SSH_HOST_KEY from ssh-keyscan against $dokku_host:$ssh_port"
-ssh-keyscan -H -p "$ssh_port" "$dokku_host" >>.ssh/known_hosts
-chmod 600 ".ssh/known_hosts"
+ssh-keyscan -H -p "$ssh_port" "$dokku_host" >>~/.ssh/known_hosts
+chmod 600 "~/.ssh/known_hosts"
 
 eval "$(ssh-agent -s)"
-ssh-add .ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 
 git_remote_url="ssh://dokku@${dokku_host}:22/${app_name}"
 
