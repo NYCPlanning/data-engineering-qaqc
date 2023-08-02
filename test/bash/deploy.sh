11ebd01be47e53919eba2f50b8f41a95deb12a76
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 15:18:11 -0400

    github action user for commit

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 mv $APP_DIR/Dockerfile ./Dockerfile
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
 git add .
 git commit -m "deployment commit"
