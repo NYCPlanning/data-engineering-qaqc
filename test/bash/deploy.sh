76db1b0ac5c26be8a4fcdf3ca5e999361655ca09
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 13:33:04 -0400

    fix branch

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -5,5 +5,5 @@ apk add git-subtree
 git config --global --add safe.directory /__w/data-engineering-qaqc/data-engineering-qaqc
 git subtree split -P test -b deploy
 git checkout deploy
-git push "$GIT_REMOTE_URL" main --force 
+git push "$GIT_REMOTE_URL" "$deploy:refs/heads/main" --force 
 ssh -p "$SSH_PORT" "dokku@$DOKKU_HOST" apps:unlock "$APP_NAME"
