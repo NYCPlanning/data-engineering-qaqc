c666cbd1fe116627ee3c65b475e14a2bfe9a80d0
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 12:14:29 -0400

    typo in workflow, logic back to deploy script

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
-set -e
 
-mkdir -p ~/.ssh
-echo "$ssh_private_key" | tr -d '\r' > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-chmod 700 ~/.ssh
-
-ssh_port=22
-
-echo "Generating SSH_HOST_KEY from ssh-keyscan against $dokku_host:$ssh_port"
-ssh-keyscan -t rsa "ssh://dokku@${dokku_host}:22/${app_name}" >> ~/.ssh/known_hosts
-chmod 600 "~/.ssh/known_hosts"
-
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
-git_remote_url="ssh://dokku@${dokku_host}:22/${app_name}"
-
-ssh -T "dokku@${dokku_host}"
-
-git push "$git_remote_url" `git subtree split --prefix test` --force
+apk add git-subtree
+/bin/setup-ssh
+git config --global --add safe.directory /__w/data-engineering-qaqc/data-engineering-qaqc
+git push "$GIT_REMOTE_URL" `git subtree split --prefix test` --force
