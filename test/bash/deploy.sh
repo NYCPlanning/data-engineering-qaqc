d83b3d9514ae13d159b7aa95494240c49bea9924
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 12:43:26 -0400

    force push

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -3,4 +3,4 @@
 apk add git-subtree
 /bin/setup-ssh
 git config --global --add safe.directory /__w/data-engineering-qaqc/data-engineering-qaqc
-git subtree push --prefix test "$GIT_REMOTE_URL" main
+git push --force "$GIT_REMOTE_URL" `git subtree split --prefix test`:main
