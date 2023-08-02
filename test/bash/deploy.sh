fc539865f2649987131466580b073554d30bcfca
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 13:08:31 -0400

    revert to no branch specified in subtree

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -4,5 +4,5 @@ apk add git-subtree
 /bin/setup-ssh
 git config --global --add safe.directory /__w/data-engineering-qaqc/data-engineering-qaqc
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-git push --force "$GIT_REMOTE_URL" `git subtree split -P test -b $branch`:$APP_REMOTE_BRANCH
+git push --force "$GIT_REMOTE_URL" `git subtree split -P test`:$APP_REMOTE_BRANCH
 ssh -p "$SSH_PORT" "dokku@$DOKKU_HOST" apps:unlock "$APP_NAME"
