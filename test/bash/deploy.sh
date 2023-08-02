c020eee663e61c43c3eda664c2a9d65c4a79ef13
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 10:00:12 -0400

    cleanup

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -e
 
-mkdir -p /root/.ssh
-echo "$ssh_private_key" | tr -d '\r' > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-chmod 700 /root/.ssh
+mkdir -p .ssh
+echo "$ssh_private_key" | tr -d '\r' > .ssh/id_rsa
+chmod 600 .ssh/id_rsa
+chmod 700 .ssh
 
 ssh_port=22
 
 log-info "Generating SSH_HOST_KEY from ssh-keyscan against $dokku_host:$ssh_port"
-ssh-keyscan -H -p "$ssh_port" "$dokku_host" >>/root/.ssh/known_hosts
-chmod 600 "/root/.ssh/known_hosts"
+ssh-keyscan -H -p "$ssh_port" "$dokku_host" >>.ssh/known_hosts
+chmod 600 ".ssh/known_hosts"
 
 eval "$(ssh-agent -s)"
-ssh-add /root/.ssh/id_rsa
+ssh-add .ssh/id_rsa
 
 git_remote_url="ssh://dokku@${dokku_host}:22/${app_name}"
 
