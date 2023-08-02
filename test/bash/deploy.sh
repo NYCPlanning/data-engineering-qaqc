893ea0559519de7285b0989bc224c9a020aa3bd3
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-08-02 13:05:38 -0400

    fix safe dir

--- test/bash/deploy.sh
+++ test/bash/deploy.sh
@@ -2,7 +2,7 @@
 
 apk add git-subtree
 /bin/setup-ssh
-git config --global --add safe.directory .
+git config --global --add safe.directory /__w/data-engineering-qaqc/data-engineering-qaqc
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 git push --force "$GIT_REMOTE_URL" `git subtree split -P test -b $branch`:$APP_REMOTE_BRANCH
 ssh -p "$SSH_PORT" "dokku@$DOKKU_HOST" apps:unlock "$APP_NAME"
